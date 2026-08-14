390a860d8968467ed124c4de52fb2b314492dfdd
Christian Robledo <christian@masquerade-circus.net>
2026-08-14 00:10:14 -0600

    chore: use published sync-core

--- sync-core/src/browser.ts
+++
@@ -1,8 +0,0 @@
-import type { SyncRuntime, SyncRuntimeOptions } from "./types.js";
-
-async function createSyncRuntime(options: SyncRuntimeOptions): Promise<SyncRuntime> {
-  void options;
-  throw new Error("sync-core createSyncRuntime() supports Node.js only");
-}
-
-export { createSyncRuntime };
--- sync-core/src/git.ts
+++
@@ -1,2 +0,0 @@
-export { createGitBackend, classifyError } from "./backends/git-cli.js";
-export type { GitBackend, GitCliBackendOptions } from "./backends/git-cli.js";
